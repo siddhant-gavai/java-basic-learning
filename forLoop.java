6ca293989dddabb65fbb9180176b7e2d52834963
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-11 17:13:05 +0530

    perform some example using forLoop

---
+++ forLoop.java
@@ -0,0 +1,28 @@
+public class forLoop {
+    public static void main(String[] args) {
+        // for(int line = 1 ; line <= 4 ; line ++ ) {
+        //     System.out.println("****");
+        // }
+
+// reverse counting
+
+// for(int number = 100 ; number >= 1 ; number--){
+//   System.out.println(number);
+  
+//         }
+
+        
+// print reaverse of a number 
+
+int n = 3104 ; 
+
+while(n > 0) {
+    int lastDigit = n%10 ; 
+    System.out.print(lastDigit);
+    n = n/10 ; // or  n/=10 ; 
+
+}
+  System.out.println();
+    
+    }
+}
