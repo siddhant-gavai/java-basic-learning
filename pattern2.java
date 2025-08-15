14b9f7c67f89fba73f5cc996e697febf4e6552e3
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-15 10:10:18 +0530

    learn 2n pattern

--- pattern2.java
+++ pattern2.java
@@ -1,18 +1,18 @@
-public class pattern {
+public class pattern2 {
     public static void main(String[] args) {
+        int n = 4;
         for (int line = 1; line <= 4; line++) {
-            for (int star = 1; star <= line; star++) {
+            for (int star = 1; star <= n - line + 1; star++) {
                 System.out.print("*");
-
             }
             System.out.println();
-
         }
     }
 }
 
 // Output
-// *
-// **
-// ***
+
 // ****
+// ***
+// **
+// *
